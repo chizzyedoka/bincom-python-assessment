fdba8d2d73c13435aa99f87f9b25c04d0bb09034
CHISOM EDOKA <edokachisom48@gmail.com>
2024-05-31 14:52:00 +0100

    restructured app

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASS = '123456'
-DB_NAME = 'bincom_test'
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
 
 # Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
